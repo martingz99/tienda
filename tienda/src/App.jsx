0973dd73dac9a205f8aa7fb81cca6cca7aec8521
martingz99 <martudev@gmail.com>
2024-08-21 19:04:33 -0300

    segundo

--- tienda/src/App.jsx
+++ tienda/src/App.jsx
@@ -2,18 +2,66 @@
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const saludando = () => {
+    alert("Hiciste click")
+  }
 
   return (
     <>
-      <div>
-        
+      <div id="app">
+        <header>
+          <div className="logo">
+              <img src="/img/nike.jpg" alt="naiki"/>
+              <span className='naiki'>Naiki</span>
+              <button className='naiki' onClick={saludando}>nenene</button>
+          </div>
+        <nav>
+            <ul>
+                <li><a href="#">Hombre</a>
+                    <ul className="dropdown">
+                        <li><a href="#">Remeras</a></li>
+                        <li><a href="#">Calzado </a></li>
+                        <li><a href="#">Moda</a></li>
+                        <li><a href="#">Gym</a></li>
+                        <li><a href="#">Moda</a></li>
+                        <li><a href="#">Moda</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Mujer</a>
+                    <ul className="dropdown">
+                        <li><a href="#">Remeras</a></li>
+                        <li><a href="#">Calzado </a></li>
+                        <li><a href="#">Moda</a></li>
+                        <li><a href="#">Gym</a></li>
+                        <li><a href="#">Moda</a></li>
+                        <li><a href="#">Moda</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Niño</a>
+                    <ul className="dropdown">
+                        <li><a href="#">Remeras</a></li>
+                        <li><a href="#">Calzado </a></li>
+                        <li><a href="#">Moda</a></li>
+                        <li><a href="#">Gym</a></li>
+                        <li><a href="#">Moda</a></li>
+                        <li><a href="#">Moda</a></li>
+                    </ul>
+                </li>
+                <li><a href="#">Accesorios</a>
+                    <ul className="dropdown">
+                        <li><a href="#">Remeras</a></li>
+                        <li><a href="#">Calzado </a></li>
+                        <li><a href="#">Moda</a></li>
+                        <li><a href="#">Gym</a></li>
+                        <li><a href="#">Moda</a></li>
+                        <li><a href="#">Moda</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </nav>
+        </header>
       </div>
-     
-      <div className="card">
-       
-      </div>
-     
     </>
   )
 }
