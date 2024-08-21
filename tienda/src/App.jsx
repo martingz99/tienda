b61c07d7a5ab81929a45a975d9f32c6ead15030d
martin gomez <martudev@gmail.com>
2024-08-21 13:49:52 -0300

    primer commit

---
+++ tienda/src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        
+      </div>
+     
+      <div className="card">
+       
+      </div>
+     
+    </>
+  )
+}
+
+export default App
